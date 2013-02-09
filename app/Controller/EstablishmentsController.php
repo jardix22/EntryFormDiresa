91a6ca1dd258176932be0ee2383dc3f112fe7648
joseph <jardix22@gmail.com>
2013-02-09 02:29:02 -0500

    establishment corregido

--- app/Controller/EstablishmentsController.php
+++ app/Controller/EstablishmentsController.php
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 <?php
-=======
-	<?php
->>>>>>> 818d509f7600479335f5b8bc372283dbf0cbefa1
+
 /**
 * EstablishmentsController
 */
@@ -33,19 +30,10 @@ class EstablishmentsController extends AppController
 
 		$data = array();
 		foreach ($establishments as $key => $establishment) {
-<<<<<<< HEAD
-			//if ( count($establishment['Vacancy']) > 0) {
-				$data[$key] = array(
-					'id' => $establishment['Establishment']['id'],
-					'name' => $establishment['Establishment']['name']			
-				);
-			//}
-=======
 			$data[$key] = array(
 				'id' => $establishment['Establishment']['id'],
 				'name' => $establishment['Establishment']['name']			
 			);
->>>>>>> 818d509f7600479335f5b8bc372283dbf0cbefa1
 		}
 
 		$this->set('establishments', $data);
